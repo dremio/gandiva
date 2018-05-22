e5059ada84e649069729e6e4ff3aa2b5770de047
Pindikura Ravindra <ravindra@dremio.com>
2018-05-22 23:27:54 +0530

    remove constexpr in definition, gcc doesn't like this.

--- cpp/src/gandiva/codegen/engine.cc
+++ cpp/src/gandiva/codegen/engine.cc
@@ -39,7 +39,7 @@
 
 namespace gandiva {
 
-constexpr char Engine::kLibPreCompiledIRDir[] = "/tmp/";
+const char Engine::kLibPreCompiledIRDir[] = "/tmp/";
 bool Engine::init_once_done_ = false;
 std::once_flag init_once_flag;
 
