2d02649d84e5cfedddb540003b14f588b4ff1126
Pindikura Ravindra <p.ravindra@yahoo.com>
2018-05-29 01:45:30 +0530

    Bootstrap evaluation using LLVM code generation (#1)
    Bootstrap evaluation using llvm code generation
    
    LLVM code generation is done using a mix of :
    - glue IR code that loops over the vector, generates function
      calls (and)
    - byte-code files generated from simple c++ functions using 
      clang (emit-llvm).
    The glue-code and pre-compiled byte code are merged and 
    optimized together.
    
    Expressions are specified using a "tree builder" where each 
    node is an arrow vector, or a binary/unary function.
    
    During code generation, the expressions are "decomposed" so 
    that the value array and bitmap array are evaluated separately 
    to compute the expression result. This avoids the use of too 
    many branch/conditional instructions (checks for "if null"), and
    hence, can be vectorized efficiently.
    
    Support added for arithmetic and logical expressions on 
    numeric types.
    
    Travis CI support added for build on ubuntu.

---
+++ ci/travis_before_script_cpp.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2017-2018 Dremio Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+source $TRAVIS_BUILD_DIR/ci/travis_env_common.sh
+
+using_homebrew=no
+
+while true; do
+    case "$1" in
+        --homebrew)
+            using_homebrew=yes
+            shift ;;
+        *) break ;;
+    esac
+done
+
+if [ "$only_library_mode" == "no" ]; then
+  source $TRAVIS_BUILD_DIR/ci/travis_install_conda.sh
+fi
+
+CMAKE_COMMON_FLAGS="\
+-DCMAKE_INSTALL_PREFIX=$GANDIVA_CPP_INSTALL"
+CMAKE_LINUX_FLAGS=""
+CMAKE_OSX_FLAGS=""
+
+if [ "$GANDIVA_TRAVIS_USE_TOOLCHAIN" == "1" ]; then
+  # Set up C++ toolchain from conda-forge packages for faster builds
+  source $TRAVIS_BUILD_DIR/ci/travis_install_toolchain.sh
+  CMAKE_COMMON_FLAGS="${CMAKE_COMMON_FLAGS}"
+fi
+
+mkdir -p $GANDIVA_CPP_BUILD_DIR
+pushd $GANDIVA_CPP_BUILD_DIR
+
+# Use Ninja for faster builds when using toolchain
+if [ $GANDIVA_TRAVIS_USE_TOOLCHAIN == "1" ]; then
+  CMAKE_COMMON_FLAGS="$CMAKE_COMMON_FLAGS -GNinja"
+fi
+
+if [ $TRAVIS_OS_NAME == "linux" ]; then
+    cmake $CMAKE_COMMON_FLAGS \
+          $CMAKE_LINUX_FLAGS \
+          -DCMAKE_BUILD_TYPE=$GANDIVA_BUILD_TYPE \
+          -DBUILD_WARNING_LEVEL=$GANDIVA_BUILD_WARNING_LEVEL \
+          $GANDIVA_CPP_DIR
+else
+    if [ "$using_homebrew" = "yes" ]; then
+        # build against homebrew's boost if we're using it
+        export BOOST_ROOT=/usr/local/opt/boost
+    fi
+    cmake $CMAKE_COMMON_FLAGS \
+          $CMAKE_OSX_FLAGS \
+          -DCMAKE_BUILD_TYPE=$GANDIVA_BUILD_TYPE \
+          -DBUILD_WARNING_LEVEL=$GANDIVA_BUILD_WARNING_LEVEL \
+          $GANDIVA_CPP_DIR
+fi
+
+# Build and install libraries
+$TRAVIS_MAKE -j4
+
+#TODO
+#$TRAVIS_MAKE install
+
+popd
---
+++ ci/travis_install_toolchain.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+  
+# Copyright (C) 2017-2018 Dremio Corporation
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source $TRAVIS_BUILD_DIR/ci/travis_env_common.sh
+
+source $TRAVIS_BUILD_DIR/ci/travis_install_conda.sh
+
+if [ ! -e $CPP_TOOLCHAIN ]; then
+    # Set up C++ toolchain from conda-forge packages for faster builds
+    conda create -y -q -p $CPP_TOOLCHAIN python=2.7 \
+        flatbuffers \
+        gflags \
+        gtest \
+        ccache \
+        cmake \
+        curl \
+        ninja \
+        arrow-cpp \
+        boost-cpp
+
+    conda update -y -q -p $CPP_TOOLCHAIN ca-certificates -c defaults
+fi
