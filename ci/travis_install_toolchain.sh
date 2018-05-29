ae26b699c41c495529252018e27ca56c85f39d86
Pindikura Ravindra <ravindra@dremio.com>
2018-05-29 18:18:33 +0530

    GDV-42 : Switch to clang as the compiler

--- ci/travis_install_toolchain.sh
+++ ci/travis_install_toolchain.sh
@@ -28,7 +28,6 @@ if [ ! -e $CPP_TOOLCHAIN ]; then
         cmake \
         curl \
         ninja \
-        arrow-cpp \
         boost-cpp
 
     conda update -y -q -p $CPP_TOOLCHAIN ca-certificates -c defaults
