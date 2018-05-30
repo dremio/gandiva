dba59e0fe7b0ffba79c3b568a4102c2507a13880
Pindikura Ravindra <ravindra@dremio.com>
2018-05-30 23:22:11 +0530

    GDV-42: Install arrow too in travis

--- ci/travis_install_arrow.sh
+++ ci/travis_install_arrow.sh
@@ -4,6 +4,11 @@ set -ex
 
 ARROW_VERSION=0.9.0
 
+# Use Ninja for faster builds when using toolchain
+if [ $GANDIVA_TRAVIS_USE_TOOLCHAIN == "1" ]; then
+  CMAKE_ARROW_FLAGS="$CMAKE_COMMON_FLAGS -GNinja"
+fi
+
 wget https://github.com/apache/arrow/archive/apache-arrow-${ARROW_VERSION}.zip
 unzip apache-arrow-${ARROW_VERSION}.zip
 
@@ -12,7 +17,7 @@ mkdir $ARROW_SRC_DIR/cpp/build
 
 pushd $ARROW_SRC_DIR/cpp/build
 
-cmake ..
+cmake $CMAKE_ARROW_FLAGS ..
 
 # Build and install libraries
 $TRAVIS_MAKE -j4
