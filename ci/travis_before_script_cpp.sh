a090ab2ddf585e195b66ce9022873a790270196c
Pindikura Ravindra <p.ravindra@yahoo.com>
2018-06-08 22:24:49 +0530

    GDV-49: [C++] switch to /// or // style comments
    Also, added "Adapted from XX" comments in ci/travis

--- ci/travis_before_script_cpp.sh
+++ ci/travis_before_script_cpp.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Adapted from Apache Arrow
+
 set -ex
 
 source $TRAVIS_BUILD_DIR/ci/travis_env_common.sh
