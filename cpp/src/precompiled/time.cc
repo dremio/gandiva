8c597c324312f6aa4c597795387baf288ebbc550
Pindikura Ravindra <ravindra@dremio.com>
2018-06-11 09:46:23 +0530

    GDV-46: [C++] fix gcc build

--- cpp/src/precompiled/time.cc
+++ cpp/src/precompiled/time.cc
@@ -29,7 +29,6 @@ extern "C" {
 
 // Extract  year.
 #define EXTRACT_YEAR(TYPE) \
-  __attribute__((always_inline)) \
   int64 extractYear##_##TYPE(TYPE millis) { \
     time_t tsec = (time_t) MILLIS_TO_SEC(millis); \
     struct tm tm; \
@@ -40,7 +39,6 @@ extern "C" {
 DATE_TYPES(EXTRACT_YEAR)
 
 #define EXTRACT_MONTH(TYPE) \
-  __attribute__((always_inline)) \
   int64 extractMonth##_##TYPE(TYPE millis) { \
     time_t tsec = (time_t) MILLIS_TO_SEC(millis); \
     struct tm tm; \
@@ -51,7 +49,6 @@ DATE_TYPES(EXTRACT_YEAR)
 DATE_TYPES(EXTRACT_MONTH)
 
 #define EXTRACT_DAY(TYPE) \
-  __attribute__((always_inline)) \
   int64 extractDay##_##TYPE(TYPE millis) { \
     time_t tsec = (time_t) MILLIS_TO_SEC(millis); \
     struct tm tm; \
@@ -62,7 +59,6 @@ DATE_TYPES(EXTRACT_MONTH)
 DATE_TYPES(EXTRACT_DAY)
 
 #define EXTRACT_HOUR(TYPE) \
-  __attribute__((always_inline)) \
   int64 extractHour##_##TYPE(TYPE millis) { \
     time_t tsec = (time_t) MILLIS_TO_SEC(millis); \
     struct tm tm; \
@@ -73,7 +69,6 @@ DATE_TYPES(EXTRACT_DAY)
 DATE_TYPES(EXTRACT_HOUR)
 
 #define EXTRACT_MINUTE(TYPE) \
-  __attribute__((always_inline)) \
   int64 extractMinute##_##TYPE(TYPE millis) { \
     time_t tsec = (time_t) MILLIS_TO_SEC(millis); \
     struct tm tm; \
