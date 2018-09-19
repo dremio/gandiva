54f2b0706bf809833d70c5e8e83eed9ff269f617
Praveen <praveen@dremio.com>
2018-09-19 12:09:20 +0530

    GDV-8:[CPP]Fix for gcc.

--- cpp/src/precompiled/arithmetic_ops.cc
+++ cpp/src/precompiled/arithmetic_ops.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <memory>
+
 #include "../codegen/error_holder.h"
 
 extern "C" {
